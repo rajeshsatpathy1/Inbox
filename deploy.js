d5d1f3a913fe1f6b20302a117d9fed8af5b61720
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-08-05 19:54:14 -0500

    Add contract deployer
    - Use the truffle hdwallet provider to connect to metamask and alchemy faucet addresses.
    - Follow the same instructions form the local testnet, but with this time with Sepolia TestNet
    - Deploy contract into Sepolia TestNet and check on https://sepolia.etherscan.io

--- deploy.js
+++ deploy.js
@@ -1,1 +1,27 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'diet snow neck yellow urban fashion omit above term robot enemy cactus',
+    'https://eth-sepolia.g.alchemy.com/v2/VLvHCP0vCSgBezMWUiijnTv5_ruucnGl'
+);
+
+const web3 = new Web3(provider);
+
+(async() => {
+    try{
+        const accounts = await web3.eth.getAccounts();
+        
+        console.log("Attempting to deploy from account", accounts[0]);
+
+        const result = await new web3.eth.Contract(JSON.parse(interface))
+            .deploy({ data: bytecode, arguments: ["Hello there!"] })
+            .send({ gas: 1000000, from: accounts[0]});
+
+        console.log("Contract deployed to", result.options.address);
+    }catch(err){
+        console.log("What the error?", err);
+    }
+    provider.engine.stop();
+})();
